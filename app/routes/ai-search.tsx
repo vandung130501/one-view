eb16cde698df6ecd059f9d5914d73b6ad91c6a6b
aBo <baobn@omegatheme.com>
2025-06-07 18:46:00 +0700

    [Task] API Raw content in Wiki

--- app/routes/ai-search.tsx
+++ app/routes/ai-search.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import SearchConsole from "~/components/SearchConsole";
 
 export default function AiSearch() {
+    // const [rawData, setRawData] = useState([]);
+    //   const [error, setError] = useState<string | null>(null);
+    // async function fetchFullTasklists() {
+    //     const res = await fetch('/api/wiki-raw-content');
     
+    //     if (!res.ok) {
+    //       throw new Error('Failed to fetch full tasklists');
+    //     }
+    
+    //     const data = await res.json();
+    //     return data;
+    //   }
+    
+    //   useEffect(() => {
+    //     fetchFullTasklists()
+    //       .then((data) => setRawData(data.tasklists))
+    //       .catch((err) => setError(err.message));
+    //   }, []);
+    
+    //   console.log("rawData: ", rawData)
     return (
         <div>
             <h1 className="text-xl font-bold">
